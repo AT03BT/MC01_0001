0050a6a837eab0d9c09ffb0fe9966695175c6e82
AT03BT <software@at-bespoke.net>
2025-04-19 10:59:39 +1000

    Update version and licensing info in MoviesController.cs
    Updated the comment header in `MoviesController.cs` to
    reflect the new version number (1.0.0) and added
    additional attribution and licensing details.

--- MC01_0001/Controllers/MoviesController.cs
+++ MC01_0001/Controllers/MoviesController.cs
@@ -1,6 +1,6 @@
 ﻿/*
     Controllers/MoviesController.cs
-    Version: 1.0.0
+    Version: 0.2.1
     (c) 2024, Minh Tri Tran, with assistance from Google's Gemini - Licensed under CC BY 4.0
     https://creativecommons.org/licenses/by/4.0/
 
